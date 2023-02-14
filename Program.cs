2a3007406f69473332c2cf39b4706c87f2ff1c86
Valerij Nikitin <Ninja15@yandex.ru>
2023-02-14 23:47:47 +0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using System.Security.Claims;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication("Bearer").AddJwtBearer();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapGet("/", () => "Hello, World!");
+app.MapGet("/secret", (ClaimsPrincipal user) => $"Hello {user.Identity?.Name}. My secret")
+    .RequireAuthorization();
+app.MapGet("/secret-claims", (ClaimsPrincipal user) => $"Hello {user.Identity?.Name}. My secret claims")
+    .RequireAuthorization();
+app.MapGet("/secret-manager", (ClaimsPrincipal user) => $"Hello {user.Identity?.Name}. My secret role")
+    .RequireAuthorization(p => p.RequireRole("Manager"));
+
+app.Run();
